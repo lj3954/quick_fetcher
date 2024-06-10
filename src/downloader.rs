d76ea0fd953b40600d8affed74a8aefdb0e1f22d
Liam <33645555+lj3954@users.noreply.github.com>
2024-06-10 12:45:51 -0700

    Correct URL filter

--- src/downloader.rs
+++ src/downloader.rs
@@ -174,7 +174,7 @@ const SINGLETHREADED_URLS: [&str; 2] = ["cdimage.ubuntu.com", "dl.sourceforge.ne
 fn choose_threads(length: Option<u64>, url: &Url) -> Option<u8> {
     if url
         .host_str()
-        .map_or(false, |host| SINGLETHREADED_URLS.iter().any(|&single| single.contains(host)))
+        .map_or(false, |host| SINGLETHREADED_URLS.iter().any(|&single| host.contains(single)))
     {
         return Some(1);
     }
